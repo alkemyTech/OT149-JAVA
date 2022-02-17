9c0df9142ae2e83500a03fe3f99515fe0bf9ae79
RodriDesmond <c.rodri92@gmail.com>
2022-02-17 19:25:49 -0300

    refactor: refactoring Members model and Testimonials model

--- src/main/java/com/alkemy/ong/model/Members.java
+++ src/main/java/com/alkemy/ong/model/Members.java
@@ -1,39 +1,57 @@
 package com.alkemy.ong.model;
 
-import com.sun.istack.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
+@Where(clause = "isActive = true")
+@SQLDelete(sql = "UPDATE users_table SET isActive=false WHERE id = ?")
 public class Members {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@NotNull
+	@NonNull
 	private String name;
 
 	private String facebookUrl;
 	private String instagramUrl;
 	private String linkedinUrl;
-	@NotNull
+	@NonNull
 	private String image;
 	private String description;
 	@CreationTimestamp
-	private LocalDateTime timestamp;
-	private boolean sofDelete = false;
+	private LocalDateTime createAt;
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	@Column(name = "is_active", nullable = false)
+	private boolean isActive = true;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		Members members = (Members) o;
+		return id != null && Objects.equals(id, members.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
--- src/main/java/com/alkemy/ong/model/Testimonials.java
+++ src/main/java/com/alkemy/ong/model/Testimonials.java
@@ -1,34 +1,50 @@
 package com.alkemy.ong.model;
 
-import com.sun.istack.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+import org.springframework.lang.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
 @ToString
+@Where(clause = "isActive = true")
+@SQLDelete(sql = "UPDATE users_table SET isActive=false WHERE id = ?")
 public class Testimonials {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@NotNull
+	@NonNull
 	private String name;
 	private String image;
 	private String content;
 	@CreationTimestamp
-	private LocalDateTime timestamp;
-	private boolean softDelete = false;
+	private LocalDateTime createAt;
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	@Column(name = "is_active", nullable = false)
+	private boolean isActive = true;
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		Testimonials that = (Testimonials) o;
+		return id != null && Objects.equals(id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
