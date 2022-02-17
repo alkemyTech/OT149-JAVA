ac7a740b96969beb8f97ac9939d72c820dac9cd3
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-02-16 23:26:06 -0300

    fix: soft delete category

--- src/main/java/com/alkemy/ong/entity/Categories.java
+++ src/main/java/com/alkemy/ong/entity/Categories.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,24 +14,31 @@ import java.time.LocalDate;
 @Entity
 @Data
 @EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE categories SET isActive = false WHERE id=?")
+@Where(clause = "isActive=true")
 @Table(name = "categories")
 public class Categories {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     private String description;
+
     private String image;
+
     @Column(name = "created_date", updatable = false, nullable = false)
     @CreatedDate
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private LocalDate createdDate;
+
     @Column(name = "modified_date")
     @LastModifiedDate
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private LocalDate modifiedDate;
+
     @Column(name = "is_active")
-    private boolean isActive=Boolean.TRUE;
+    private boolean isActive = Boolean.TRUE;
 }
