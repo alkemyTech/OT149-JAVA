19ab4755ac9b363b170c15317f7454073989d320
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2022-02-19 16:13:23 -0300

    Feature/OT149-20/22: Testimonials & Members model and repository  (#2)
    * feature:
    
    added OT149-20: Testimonials model and repository
    added OT149-22: Members model and repository
    
    * refactor: refactoring Members model and Testimonials model
    
    * Fix: Member model and Testimonials model
    
    Typo: isActive should be as is_active.
    Used Lombok's @ Data
    Changed wildcard character (*) for imports.
    
    * Update application.properties
    
    * Fix createdAt and updatedAt for Members and Testimonials entities
    
    Members:
    -Fixed createdAt
    -Fixed updateAt
    Testimonials
    -Fixed createdAt
    -Fixed updateAt
    
    * fix: updatedAt nullable = false
    
    * fixing .gitignore
    
    * fix: pom.xml and application.properties

---
+++ src/main/java/com/alkemy/ong/model/Members.java
@@ -0,0 +1,48 @@
+package com.alkemy.ong.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Where(clause = "is_active = true")
+@SQLDelete(sql = "UPDATE users_table SET is_active=false WHERE id = ?")
+@NoArgsConstructor
+public class Members {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotNull
+	private String name;
+	private String facebookUrl;
+	private String instagramUrl;
+	private String linkedinUrl;
+	@NotNull
+	private String image;
+	private String description;
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate createdAt;
+	@LastModifiedDate
+	@Column(nullable = false)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate updatedAt;
+	private boolean isActive = true;
+}
