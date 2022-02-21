29c6736366b2e66b0500fac7945e1eb3618bce0b
Alberto Saiz Guidarelli <83599107+alberto2331@users.noreply.github.com>
2022-02-21 18:51:53 -0300

    Feature/ot149-15: organization entity and repository (#4)
    * squash
    
    * organization entity corrected
    
    * optimize imports
    
    Co-authored-by: Jonathan Manera <manerajona@gmail.com>

---
+++ src/main/java/com/alkemy/ong/model/Organizations.java
@@ -0,0 +1,50 @@
+package com.alkemy.ong.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE organizations SET is_active=false WHERE id = ?")
+@Where(clause="isActive=true")
+public class Organizations {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;	
+	private String name;
+	private String images;
+	private String addres;
+	private int phone;
+	private String email;
+	private  String welcomeText;
+	private String aboutUsText;
+	
+	
+	@Column( name ="updated_at", nullable = false)
+	@LastModifiedDate
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate updatedAt;
+	
+	@Column(name= "created_at", updatable= false ,nullable=false)
+	@CreatedDate
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate createdAt;	
+
+	@Column(name ="is_active")
+	private boolean isActive = Boolean.TRUE;
+
+}
