b3a3231ed2b1a612a18208ebaad336cae7c2f1d2
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-02-23 14:06:29 -0300

    fix: uncomment categoryId

--- src/main/java/com/alkemy/ong/model/New.java
+++ src/main/java/com/alkemy/ong/model/New.java
@@ -9,11 +9,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -41,8 +45,6 @@ public class New {
     @Column(name = "is_active")
     private boolean isActive=Boolean.TRUE;
 
-    /*
     @ManyToMany(mappedBy = "news", fetch = FetchType.LAZY)
-    private List<Categories> categoryId = new ArrayList<>();
-     */
+    private List<Category> categoryId = new ArrayList<>();
 }
