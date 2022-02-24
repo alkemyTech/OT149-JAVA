525ec4ac9ec3e9f93ac89f9d1dc4bc017d1408da
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-02-24 12:38:32 -0300

    fix: refactor NewServiceImp to NewServiceImpl

--- src/main/java/com/alkemy/ong/mapper/NewMapper.java
+++ src/main/java/com/alkemy/ong/mapper/NewMapper.java
@@ -1,10 +1,10 @@
 package com.alkemy.ong.mapper;
 
 import com.alkemy.ong.dto.NewDetailDto;
+import com.alkemy.ong.model.New;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface NewMapper {
-    New toNew(NewDetailDto dto);
     NewDetailDto toNewDetailDto(New newModel);
 }
--- src/main/java/com/alkemy/ong/service/impl/NewServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewServiceImpl.java
@@ -2,13 +2,17 @@ package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.NewDetailDto;
 import com.alkemy.ong.mapper.NewMapper;
+import com.alkemy.ong.model.New;
+import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.NewService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NewServiceImp implements NewService {
-    private NewRepository repository;
+public class NewServiceImpl implements NewService {
+
+    private NewsRepository repository;
     private NewMapper mapper;
+
     public NewDetailDto getNewById (Long id){
         New newModel = repository.getById(id);
         NewDetailDto result = mapper.toNewDetailDto(newModel);
