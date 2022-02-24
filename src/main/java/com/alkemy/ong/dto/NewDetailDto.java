b15b0d85110fe3a20c99c35e3defd1b2dc9f0142
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-02-24 18:28:20 -0300

    fix: NewDetailDto

--- src/main/java/com/alkemy/ong/dto/NewDetailDto.java
+++ src/main/java/com/alkemy/ong/dto/NewDetailDto.java
@@ -4,7 +4,6 @@ import com.alkemy.ong.model.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.sql.Timestamp;
 
 @Data
 @NoArgsConstructor
@@ -13,8 +12,5 @@ public class NewDetailDto {
     private String name;
     private String content;
     private String image;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
-    private boolean isActive;
     private Category categoryId;
 }
