70af36ff0eb68f501adc4b6ec47b7d671a03efbe
RodriDesmond <c.rodri92@gmail.com>
2022-02-25 21:04:07 -0300

    Update ImageUploadService.java
    Configured Admin's only access to ImageUploadService with @Secured annotation

--- src/main/java/com/alkemy/ong/service/ImageUploadService.java
+++ src/main/java/com/alkemy/ong/service/ImageUploadService.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.service;
 
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -14,5 +15,6 @@ public interface ImageUploadService {
 
 	void uploadFileTos3bucket(String fileName, File file);
 
+	@Secured("ROLE_ADMIN")
 	String uploadImage(MultipartFile multipartFile) throws FileUploadException;
 }
