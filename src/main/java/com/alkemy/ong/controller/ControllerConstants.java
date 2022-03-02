5a26bb63be01033559d9987f70cab24130c12076
RodriDesmond <c.rodri92@gmail.com>
2022-03-02 20:09:01 -0300

    Fix: Typo on ControllerConstants

--- src/main/java/com/alkemy/ong/controller/ControllerConstants.java
+++ src/main/java/com/alkemy/ong/controller/ControllerConstants.java
@@ -4,6 +4,6 @@ public interface ControllerConstants {
     String V_1_USERS = "/v1/users";
     String V_1_NEWS = "/v1/news";
     String V_1_AUTH = "/v1/auth";
-    String V_1_TESTIMONIAL = "/v1/testimonial";
+    String V_1_TESTIMONIAL = "/v1/testimonials";
     String REQ_MAPP_ACTIVITIES = "/v1/activities";
 }
