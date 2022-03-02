615d2f1cdae9df6e87411dbc0b098a87624d1964
Lautaro Yanzon <88438420+lautayanzon@users.noreply.github.com>
2022-03-02 12:58:38 -0300

    Feature OT149-30: welcome email (#24)
    * feat: send welcome email integrated
    
    * fix: autowired annotation removed

--- src/main/java/com/alkemy/ong/controller/ControllerConstants.java
+++ src/main/java/com/alkemy/ong/controller/ControllerConstants.java
@@ -2,6 +2,6 @@ package com.alkemy.ong.controller;
 
 public interface ControllerConstants {
     String V_1_USERS = "/v1/users";
-    String V_1_AUTH = "/auth";
+    String V_1_AUTH = "/v1/auth";
     String REQ_MAPP_ACTIVITIES = "/v1/activities";
 }
