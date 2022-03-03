bbbab36f9a553532cf93fe6b4c3c8ebbb61df7b8
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-03-03 02:37:06 -0300

    feat: delete category

--- src/main/java/com/alkemy/ong/controller/ControllerConstants.java
+++ src/main/java/com/alkemy/ong/controller/ControllerConstants.java
@@ -5,4 +5,5 @@ public interface ControllerConstants {
     String V_1_NEWS = "/v1/news";
     String V_1_AUTH = "/v1/auth";
     String REQ_MAPP_ACTIVITIES = "/v1/activities";
+    String V_1_CATEGORIES = "/v1/categories";
 }
