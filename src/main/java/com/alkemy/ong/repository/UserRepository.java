ae6c2afd772a7d55bc892c20388c390e20095c27
RodriDesmond <c.rodri92@gmail.com>
2022-03-02 23:04:13 -0300

    feature: GET logged user info
    Added getUserLogged method and implementation
    Added  getUserLogget Get Request in AuthController

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
 }
 
