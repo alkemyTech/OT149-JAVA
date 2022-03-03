2bbaa1f33f29e230406dea29d6a86dab5aba6117
RodriDesmond <c.rodri92@gmail.com>
2022-03-02 22:48:22 -0300

    Update TestimonialService.java

--- src/main/java/com/alkemy/ong/service/TestimonialService.java
+++ src/main/java/com/alkemy/ong/service/TestimonialService.java
@@ -1,8 +1,9 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.TestimonialDto;
+import org.springframework.security.access.annotation.Secured;
 
 public interface TestimonialService {
-
+	@Secured("ROLE_ADMIN")
 	void saveTestimonial(TestimonialDto dto);
 }
