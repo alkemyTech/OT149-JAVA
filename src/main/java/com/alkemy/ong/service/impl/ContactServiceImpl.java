a7119e7d588c6556c49a721b690aab336b152280
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-03 12:42:06 -0300

    feat: created contact service and contact DTO

---
+++ src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.service.impl;
+
+import com.alkemy.ong.dto.ContactDto;
+import com.alkemy.ong.service.ContactService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+
+    @Override
+    public ContactDto saveContact(ContactDto dto) {
+        return null;
+    }
+
+    @Override
+    public List<ContactDto> getAll() {
+        return null;
+    }
+}
