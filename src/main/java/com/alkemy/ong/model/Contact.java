18ee0166c6f9e3989153857c080bef88fceb464f
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-03 12:02:10 -0300

    feat: created contact model

---
+++ src/main/java/com/alkemy/ong/model/Contact.java
@@ -0,0 +1,63 @@
+package com.alkemy.ong.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE contacts SET deleted_at=CURDATE() WHERE id = ?")
+@Where(clause = "deleted_at=NULL")
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Lob
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate updatedAt;
+
+    @Column(name = "deleted_at")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate deletedAt;
+}
