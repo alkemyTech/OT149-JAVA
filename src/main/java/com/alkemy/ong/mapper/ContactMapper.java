a373c97452433aeb85de3d02b14afd831c2e513b
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-03 21:14:19 -0300

    feat: added toContactDtoList method in ContactMapper and added body of the saveContact and getAll methods in ContactServiceImpl

--- src/main/java/com/alkemy/ong/mapper/ContactMapper.java
+++ src/main/java/com/alkemy/ong/mapper/ContactMapper.java
@@ -4,11 +4,15 @@ import com.alkemy.ong.dto.ContactDto;
 import com.alkemy.ong.model.Contact;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ContactMapper {
 
     Contact toContact(ContactDto dto);
 
     ContactDto toContactDto(Contact contact);
+
+    List<ContactDto> toContactDtoList(List<Contact> contacts);
 
 }
--- src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ContactServiceImpl.java
@@ -1,22 +1,32 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.ContactDto;
+import com.alkemy.ong.mapper.ContactMapper;
+import com.alkemy.ong.model.Contact;
+import com.alkemy.ong.repository.ContactsRepository;
 import com.alkemy.ong.service.ContactService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
 
+    private final ContactsRepository contactsRepository;
+    private final ContactMapper contactMapper;
 
     @Override
     public ContactDto saveContact(ContactDto dto) {
-        return null;
+        Contact entity = this.contactMapper.toContact(dto);
+        Contact entitySaved = this.contactsRepository.save(entity);
+        return this.contactMapper.toContactDto(entitySaved);
     }
 
     @Override
     public List<ContactDto> getAll() {
-        return null;
+        List<Contact> contacts = this.contactsRepository.findAll();
+        return this.contactMapper.toContactDtoList(contacts);
     }
 }
