1707e096d685d9958c4e9be3b8df01b45b3f4e7c
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-04 11:00:20 -0300

    feat: added POST endpoint in ContactController to save contact

--- src/main/java/com/alkemy/ong/controller/v1/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/v1/ContactController.java
@@ -1,7 +1,12 @@
 package com.alkemy.ong.controller.v1;
 
+import com.alkemy.ong.dto.ContactDto;
 import com.alkemy.ong.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,10 @@ public class ContactController {
 
     private final ContactService contactService;
 
+    @PostMapping
+    public ResponseEntity<ContactDto> saveContact(@RequestBody ContactDto dto) {
+        ContactDto result = this.contactService.saveContact(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 
 }
