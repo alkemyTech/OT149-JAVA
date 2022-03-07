aba140fc12eca6459997907d6c4adc9263d2ffaa
Lautaro Yanzon <88438420+lautayanzon@users.noreply.github.com>
2022-03-07 16:47:39 -0300

    Feat OT149-69: endpoint create and resources added (#44)

--- src/main/java/com/alkemy/ong/service/MemberService.java
+++ src/main/java/com/alkemy/ong/service/MemberService.java
@@ -2,10 +2,11 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.MemberDto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface MemberService {
 
     List<MemberDto> getAll();
+
+    Integer saveMember(MemberDto dto);
 }
