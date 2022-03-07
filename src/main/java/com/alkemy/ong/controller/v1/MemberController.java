64f2c52f064f0a57981719c77e17569ffc8d8e29
Lautaro Yanzon <lautaroyanzon13@gmail.com>
2022-03-07 12:52:50 -0300

    fix: controller method added

--- src/main/java/com/alkemy/ong/controller/v1/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/v1/MemberController.java
@@ -7,10 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static com.alkemy.ong.controller.ControllerConstants.V_1_MEMBERS;
@@ -27,4 +31,12 @@ public class MemberController {
     public List<MemberDto> getAll() {
         return memberService.getAll();
     }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateMember(@PathVariable Integer id, @Valid @RequestBody MemberDto dto) {
+        memberService.updateMember(id, dto);
+    }
+
 }
