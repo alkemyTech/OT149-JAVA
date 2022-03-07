9e71a47f0a3170f6570609d230062e2c5dd60f08
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-07 13:07:35 -0300

    Feature OT149-101: Refactorizar Error DTO (#41)
    * feat: added the classes ErrorDetails and AbstractExceptionHandler,added the enumerations Location and ApplicationErrorCode. Now GlobalControllerExceptionHandler extends of AbstractExceptionHandler
    
    * Refactor handleUserNotFound method
    
    Co-authored-by: Jonathan Manera <manerajona@gmail.com>

--- src/main/java/com/alkemy/ong/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/alkemy/ong/exception/GlobalControllerExceptionHandler.java
@@ -1,25 +1,23 @@
 package com.alkemy.ong.exception;
 
 import com.alkemy.ong.dto.UserNotFoundErrorDTO;
+import com.alkemy.ong.enumeration.ApplicationErrorCode;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.ZonedDateTime;
 import java.util.Arrays;
 
 @ControllerAdvice
-public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
-  
+public class GlobalControllerExceptionHandler extends AbstractExceptionHandler {
+
     /**
      * Este metodo se encarga de la captura del error USER_NOT_FOUND
      * @return Devuelve la excepcion que estoy capturando y el dto
      */
-
     @ExceptionHandler(value = {UserNotFoundException.class})
     protected ResponseEntity<Object> handleUserNotFound(RuntimeException ex, WebRequest request){
 
@@ -29,7 +27,7 @@ public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHan
                 Arrays.asList("User not found")
         );
 
-        return handleExceptionInternal(ex, errorDTO, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        return super.handleExceptionInternal(ex, new HttpHeaders(), HttpStatus.NOT_FOUND, request, ApplicationErrorCode.NOT_FOUND, errorDTO.getMessage());
     }
   
     /*@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -44,7 +42,7 @@ public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHan
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }*/
   
-  @ExceptionHandler(value = {NotFoundException.class})
+  /*@ExceptionHandler(value = {NotFoundException.class})
     protected ResponseEntity<Object> handleOrganizationNotFound(RuntimeException ex, WebRequest request){
 
         ErrorResponse error = new ErrorResponse();
@@ -54,6 +52,6 @@ public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHan
         error.setTimeStamp(ZonedDateTime.now());
 
         return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-    }
+    }*/
 }
 
