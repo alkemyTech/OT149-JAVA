a2802a077ef0e96e89f599ab3a99bb69450efe22
Lautaro Yanzon <88438420+lautayanzon@users.noreply.github.com>
2022-03-07 19:26:23 -0300

    Feat OT149-71: Endpoint update members (#46)
    * feat: endpoint update member added
    
    * fix: controller method added

--- src/main/java/com/alkemy/ong/controller/v1/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/v1/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,12 @@ public class MemberController {
     @GetMapping
     public List<MemberDto> getAll() {
         return memberService.getAll();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateMember(@PathVariable Integer id, @Valid @RequestBody MemberDto dto) {
+        memberService.updateMember(id, dto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
