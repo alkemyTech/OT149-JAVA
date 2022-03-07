b1159a5c43a10030a97d5f4c9d5f81d6430b7c11
Lautaro Yanzon <lautaroyanzon13@gmail.com>
2022-03-07 11:06:00 -0300

    feat: endpoint create and resources added

--- src/main/java/com/alkemy/ong/controller/v1/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/v1/MemberController.java
@@ -7,10 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static com.alkemy.ong.controller.ControllerConstants.V_1_MEMBERS;
@@ -27,4 +32,15 @@ public class MemberController {
     public List<MemberDto> getAll() {
         return memberService.getAll();
     }
+
+    @PostMapping
+    public ResponseEntity<Void> addMember(UriComponentsBuilder uriComponentsBuilder, @Valid @RequestBody MemberDto dto) {
+
+        final int memberId = memberService.saveMember(dto);
+
+        UriComponents uriComponents = uriComponentsBuilder.path("/{id}").buildAndExpand(memberId);
+
+        return ResponseEntity.created(uriComponents.toUri()).build();
+    }
+
 }
