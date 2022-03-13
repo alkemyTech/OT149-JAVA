1636d52557c5775957dee9320dff5b0dcd341359
Fer Julian <69294816+ferjulian@users.noreply.github.com>
2022-03-13 18:54:34 -0300

    Feat OT149-40: Add endpoint to get a list of Categories (#55)

--- src/main/java/com/alkemy/ong/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -2,7 +2,10 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.dto.CategoryDetailDto;
+import com.alkemy.ong.dto.CategoryListDto;
 import com.alkemy.ong.dto.CategoryPutDto;
+
+import java.util.List;
 
 public interface CategoryService  {
 
@@ -10,4 +13,6 @@ public interface CategoryService  {
     CategoryDetailDto getCategoryById(Long id);
     void deleteCategory(Long id);
     void updateCategory(Long id, CategoryPutDto putDto);
+
+    List<CategoryListDto> getAllCategories();
 }
