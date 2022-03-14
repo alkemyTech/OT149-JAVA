9ef178c90eb81067d2de2f4433b5063deeaab192
IgnacioPadovan <83479598+IgnacioPadovan@users.noreply.github.com>
2022-03-14 10:08:34 -0300

    Update SlideMapper.java

--- src/main/java/com/alkemy/ong/mapper/SlideMapper.java
+++ src/main/java/com/alkemy/ong/mapper/SlideMapper.java
@@ -8,13 +8,12 @@ import org.mapstruct.Mapping;
 
 @Mapper
 public interface SlideMapper {
-	
+
 	@Mapping(source = "slide.imageUrl", target = "imageB64")
 	@Mapping(source = "slide.organization.id", target = "organizationId")
-    SlideDto toSlideDto(Slide slide);
-	
-	@Mapping(source = "slideDto.imageB64", target = "imageUrl")
+	SlideDto toSlideDto(Slide slide);
 
-    Slide toSlide(SlideDto slideDto);
+	@Mapping(source = "slideDto.imageB64", target = "imageUrl")
+	Slide toSlide(SlideDto slideDto);
 
 }
