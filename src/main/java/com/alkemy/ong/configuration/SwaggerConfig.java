bd06e5b2d3a1a7a883e9aadc063c94cbae319b54
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-14 15:33:47 -0300

    fix: code formatting

--- src/main/java/com/alkemy/ong/configuration/SwaggerConfig.java
+++ src/main/java/com/alkemy/ong/configuration/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
 @SecurityScheme(
