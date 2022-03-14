2e303571d462b1e720e2638c4a4746085f373794
RodriDesmond <c.rodri92@gmail.com>
2022-03-14 14:48:25 -0300

    Update CommentController.java

--- src/main/java/com/alkemy/ong/controller/v1/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/v1/CommentController.java
@@ -3,12 +3,14 @@ package com.alkemy.ong.controller.v1;
 import com.alkemy.ong.service.CommentService;
 import com.alkemy.ong.dto.CommentDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -25,6 +27,7 @@ public class CommentController {
 	private final CommentService service;
 
 	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void update(
 			@PathVariable Long id,
 			@Valid @RequestBody CommentDto dto){
