bd752602ef42a3a476518265a5388e0ea8e20943
IgnacioPadovan <83479598+IgnacioPadovan@users.noreply.github.com>
2022-03-14 10:34:38 -0300

    Update SlideServiceImpl.java

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -1,31 +1,17 @@
 package com.alkemy.ong.service.impl;
 
-import com.alkemy.ong.dto.MemberDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.alkemy.ong.dto.SlideDto;
-import com.alkemy.ong.exception.NullFileException;
 import com.alkemy.ong.mapper.SlideMapper;
-import com.alkemy.ong.model.Member;
 import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.model.Slide;
 import com.alkemy.ong.repository.SlideRepository;
 import com.alkemy.ong.service.OrganizationService;
 import com.alkemy.ong.service.SlideService;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import lombok.RequiredArgsConstructor;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Base64;
-
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -34,21 +20,21 @@ public class SlideServiceImpl implements SlideService {
 	private final SlideRepository repository;
 	private final SlideMapper mapper;
 	private final AmazonS3ServiceImpl amazonService;
-	private final Base64 base64;
 	private final OrganizationService orgService;
 
 	@Transactional
 	@Override
-	public SlideDto saveSlide(SlideDto dto) throws FileUploadException {
+	public Long saveSlide(SlideDto dto) {
 
-		String fileUrl = amazonService.uploadImage64(dto.getImageB64());
+		String fileUrl;
+		fileUrl = amazonService.uploadImage64(dto.getImageB64());
 
 		Organization org = orgService.findById(dto.getOrganizationId());
-		
+
 		Slide slide = mapper.toSlide(dto);
 		slide.setImageUrl(fileUrl);
 		slide.setOrganization(org);
-		
+
 		if (slide.getOrder() == null) {
 			Integer maxOrder = repository.findMaxOrder(slide.getOrganization().getId());
 			if (maxOrder == null) {
@@ -59,9 +45,8 @@ public class SlideServiceImpl implements SlideService {
 		}
 
 		Slide saved = repository.save(slide);
-		SlideDto dtoSaved = mapper.toSlideDto(saved);
-		
-		return dtoSaved;
+
+		return saved.getId();
 	}
 
 }
