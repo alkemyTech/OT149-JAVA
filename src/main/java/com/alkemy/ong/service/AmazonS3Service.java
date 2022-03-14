adb9c6ffea776880ae54c3accc0db389f1396d6f
IgnacioPadovan <83479598+IgnacioPadovan@users.noreply.github.com>
2022-03-14 10:13:51 -0300

    Update AmazonS3Service.java

--- src/main/java/com/alkemy/ong/service/AmazonS3Service.java
+++ src/main/java/com/alkemy/ong/service/AmazonS3Service.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +12,8 @@ public interface AmazonS3Service {
 	@Secured("ROLE_ADMIN")
 	String uploadImage(MultipartFile multipartFile);
 	
-	String uploadImage64(String b64) throws FileUploadException;
+	@Secured("ROLE_ADMIN")
+	String uploadImage64(String b64);
 	
 	File b64ToFile(String b64) throws FileNotFoundException, IOException;
 	
