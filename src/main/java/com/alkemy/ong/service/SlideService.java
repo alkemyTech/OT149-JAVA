fccc70c6a01b37c9cb8296ab402769048aa13b18
IgnacioPadovan <83479598+IgnacioPadovan@users.noreply.github.com>
2022-03-14 10:31:44 -0300

    Update SlideService.java

--- src/main/java/com/alkemy/ong/service/SlideService.java
+++ src/main/java/com/alkemy/ong/service/SlideService.java
@@ -1,11 +1,9 @@
 package com.alkemy.ong.service;
 
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
-
 import com.alkemy.ong.dto.SlideDto;
 
 public interface SlideService {
 
-	SlideDto saveSlide(SlideDto dto) throws FileUploadException;
+	SlideDto saveSlide(SlideDto dto);
 	
 }
