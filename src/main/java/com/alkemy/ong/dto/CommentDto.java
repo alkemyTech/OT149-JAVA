bc2176a16576ca3b9515dfe7e64b6ce7bc1bce55
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2022-03-15 08:26:10 -0300

    Feat OT149-75: Added Comments list endpoint (#63)
    * features:
    
    Added createComment Service
    Added POST comment endpoint
    
    * Fixed saveComment method
    
    * feature:
    
    Added endpoint for comment's list
    
    * Merge branch 'main' into feature/OT149-75
    
    * fix: requested changes

--- src/main/java/com/alkemy/ong/dto/CommentDto.java
+++ src/main/java/com/alkemy/ong/dto/CommentDto.java
@@ -1,24 +1,21 @@
 package com.alkemy.ong.dto;
 
-import com.alkemy.ong.model.New;
-import com.alkemy.ong.model.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentDto {
 
 	@NotBlank(message = "Obligatory field.")
 	private String body;
-	@Column(nullable = false)
 	private Long usersId;
-	@Column(nullable = false)
 	private Long newsId;
 }
