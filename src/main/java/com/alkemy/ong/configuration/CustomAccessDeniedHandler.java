a271c7d5dd515ecbcb4e4fe2acade4e284f567d8
Gustavo Hernández <adriangh1321@gmail.com>
2022-03-14 21:17:27 -0300

    fix: unused import deleted

--- src/main/java/com/alkemy/ong/configuration/CustomAccessDeniedHandler.java
+++ src/main/java/com/alkemy/ong/configuration/CustomAccessDeniedHandler.java
@@ -8,14 +8,13 @@ import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse response, AccessDeniedException e) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(403);
         ErrorDetails error = ErrorDetails.builder()
--- src/main/java/com/alkemy/ong/configuration/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/alkemy/ong/configuration/CustomAuthenticationEntryPoint.java
@@ -3,27 +3,23 @@ package com.alkemy.ong.configuration;
 import com.alkemy.ong.enumeration.ApplicationErrorCode;
 import com.alkemy.ong.enumeration.Location;
 import com.alkemy.ong.exception.ErrorDetails;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.json.Json;
-//import javax.json.stream.JsonGenerator;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
+
 import java.io.IOException;
-import java.io.OutputStream;
+
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(403);
         ErrorDetails error = ErrorDetails.builder()
@@ -35,16 +31,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonError = objectMapper.writeValueAsString(error);
         response.getWriter().write(jsonError);
-        /*OutputStream outputStream = new ByteArrayOutputStream();
-        JsonFactory factory=new JsonFactory();
-        JsonGenerator generator = factory.createGenerator(response.getWriter());
 
-        generator.writeStartObject();
-        generator.writeFieldName("null");
-
-
-
-        generator.writeEndObject();*/
     }
 }
 
