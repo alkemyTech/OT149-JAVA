96f87117d600abf45d8ac3e4dceb00594629561a
Fer Julian <69294816+ferjulian@users.noreply.github.com>
2022-03-24 08:33:33 -0300

    Fix OT149-106: Modify ImageUploadController documentation (#100)
    * [OT149-106] Modify ImageUploadController documentation
    
    * Refactor - add constants
    
    Co-authored-by: Jonathan Manera <manerajona@gmail.com>

--- src/main/java/com/alkemy/ong/controller/v1/ImageUploadController.java
+++ src/main/java/com/alkemy/ong/controller/v1/ImageUploadController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,13 +36,13 @@ public class ImageUploadController {
 	@Operation(summary = "Add a new image to the database")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "201", description = "Create contact",
-					content = { @Content(mediaType = "application/json",
+					content = { @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
 							schema = @Schema(implementation = String.class)) }),
 			@ApiResponse(responseCode = "400", description = "Invalid field",
-					content = { @Content(mediaType = "application/json",
+					content = { @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
 							schema = @Schema(implementation = ErrorDetails.class)) }),
 			@ApiResponse(responseCode = "403", description = "Invalid token or token expired | Accessing with invalid role",
-					content = {@Content(mediaType = "application/json",
+					content = {@Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
 							schema = @Schema(implementation = ErrorDetails.class))})
 	})
 	@PostMapping
